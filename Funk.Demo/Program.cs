e3ce60d32ec5114ae38870ebcfbf85a3174e960e
Harun Ćerim <harun.cerim@outlook.com>
2020-03-08 11:39:00 +0100

    Add project files.

---
+++ Funk.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Funk.Demo
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Welcome to the world of Funk.");
+        }
+    }
+}
