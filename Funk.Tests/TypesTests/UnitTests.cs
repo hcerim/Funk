6bb1411cd4db44735e43832ba2e00047736434e8
Harun Ćerim <harun.cerim@outlook.com>
2020-03-18 00:03:14 +0100

    tests for unit finished

---
+++ Funk.Tests/TypesTests/UnitTests.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Funk.Tests.TypesTests
+{
+    public class UnitTests : Test
+    {
+        [Fact]
+        public void Get_Unit_Value()
+        {
+            UnitTest(
+                _ => Unit.Value, 
+                u => u, 
+                u => Assert.IsType<Unit>(u)
+            );
+        }
+
+        [Fact]
+        public void Unit_Transform()
+        {
+            UnitTest(
+                _ => Unit.Value,
+                u => u.TransformTo("John"),
+                s => Assert.Equal("John", s)
+            );
+        }
+
+        [Fact]
+        public void Unit_Map()
+        {
+            UnitTest(
+                _ => Unit.Value,
+                u => u.Match(_ => "John Doe"),
+                s => Assert.Equal("John Doe", s)
+            );
+        }
+
+        [Fact]
+        public void Compare_Unit()
+        {
+            UnitTest(
+                _ => Unit.Value,
+                u => u.Equals(new Unit()),
+                Assert.True
+            );
+        }
+
+        [Fact]
+        public void Unit_ToString()
+        {
+            UnitTest(
+                _ => Unit.Value,
+                u => u.ToString(),
+                Assert.Empty
+            );
+        }
+    }
+}
