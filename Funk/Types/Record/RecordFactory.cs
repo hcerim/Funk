f16218c1445864177d57687f77559e72bfaead38
Harun Ćerim <harun.cerim@outlook.com>
2020-03-17 23:45:52 +0100

    minor code refactoring (expression bodied methods and marking pure methods)

--- Funk/Types/Record/RecordFactory.cs
+++ Funk/Types/Record/RecordFactory.cs
@@ -1,86 +1,61 @@
-﻿namespace Funk
+﻿using System.Diagnostics.Contracts;
+
+namespace Funk
 {
     public struct Record
     {
         /// <summary>
         /// Creates a record of 1.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1> Create<T1>(T1 t1)
-        {
-            return new Record<T1>(t1);
-        }
+        [Pure]
+        public static Record<T1> Create<T1>(T1 t1) => new Record<T1>(t1);
 
         /// <summary>
         /// Creates a record of 2.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2> Create<T1, T2>(T1 t1, T2 t2)
-        {
-            return new Record<T1, T2>(t1, t2);
-        }
+        [Pure]
+        public static Record<T1, T2> Create<T1, T2>(T1 t1, T2 t2) => new Record<T1, T2>(t1, t2);
 
         /// <summary>
         /// Creates a record of 2.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2> Create<T1, T2>((T1 t1, T2 t2) tuple)
-        {
-            return new Record<T1, T2>(tuple);
-        }
+        [Pure]
+        public static Record<T1, T2> Create<T1, T2>((T1 t1, T2 t2) tuple) => new Record<T1, T2>(tuple);
 
         /// <summary>
         /// Creates a record of 3.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2, T3> Create<T1, T2, T3>(T1 t1, T2 t2, T3 t3)
-        {
-            return new Record<T1, T2, T3>(t1, t2, t3);
-        }
+        [Pure]
+        public static Record<T1, T2, T3> Create<T1, T2, T3>(T1 t1, T2 t2, T3 t3) => new Record<T1, T2, T3>(t1, t2, t3);
 
         /// <summary>
         /// Creates a record of 3.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2, T3> Create<T1, T2, T3>((T1 t1, T2 t2, T3 t3) tuple)
-        {
-            return new Record<T1, T2, T3>(tuple);
-        }
+        [Pure]
+        public static Record<T1, T2, T3> Create<T1, T2, T3>((T1 t1, T2 t2, T3 t3) tuple) => new Record<T1, T2, T3>(tuple);
 
         /// <summary>
         /// Creates a record of 4.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2, T3, T4> Create<T1, T2, T3, T4>(T1 t1, T2 t2, T3 t3, T4 t4)
-        {
-            return new Record<T1, T2, T3, T4>(t1, t2, t3, t4);
-        }
+        [Pure]
+        public static Record<T1, T2, T3, T4> Create<T1, T2, T3, T4>(T1 t1, T2 t2, T3 t3, T4 t4) => new Record<T1, T2, T3, T4>(t1, t2, t3, t4);
 
         /// <summary>
         /// Creates a record of 4.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2, T3, T4> Create<T1, T2, T3, T4>((T1 t1, T2 t2, T3 t3, T4 t4) tuple)
-        {
-            return new Record<T1, T2, T3, T4>(tuple);
-        }
+        [Pure]
+        public static Record<T1, T2, T3, T4> Create<T1, T2, T3, T4>((T1 t1, T2 t2, T3 t3, T4 t4) tuple) => new Record<T1, T2, T3, T4>(tuple);
 
         /// <summary>
         /// Creates a record of 5.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2, T3, T4, T5> Create<T1, T2, T3, T4, T5>(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5)
-        {
-            return new Record<T1, T2, T3, T4, T5>(t1, t2, t3, t4, t5);
-        }
+        [Pure]
+        public static Record<T1, T2, T3, T4, T5> Create<T1, T2, T3, T4, T5>(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) => new Record<T1, T2, T3, T4, T5>(t1, t2, t3, t4, t5);
 
         /// <summary>
         /// Creates a record of 5.
         /// </summary>
-        /// <returns></returns>
-        public static Record<T1, T2, T3, T4, T5> Create<T1, T2, T3, T4, T5>((T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) tuple)
-        {
-            return new Record<T1, T2, T3, T4, T5>(tuple);
-        }
+        [Pure]
+        public static Record<T1, T2, T3, T4, T5> Create<T1, T2, T3, T4, T5>((T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) tuple) => new Record<T1, T2, T3, T4, T5>(tuple);
     }
 }
--- Funk/Types/Unit/Unit.cs
+++ Funk/Types/Unit/Unit.cs
@@ -11,6 +11,18 @@ namespace Funk
     {
         public static readonly Unit Value = new Unit();
 
+        /// <summary>
+        /// Transforms Unit into a specified object.
+        /// </summary>
+        [Pure]
+        public T TransformTo<T>(T t) => t;
+
+        /// <summary>
+        /// Transforms unit into a specified object.
+        /// </summary>
+        [Pure]
+        public T TransformTo<T>(Func<Unit, T> f) => f(Value);
+
         [Pure]
         public override string ToString() => string.Empty;
 
