78af9fc4d41763df886310133c0204f31ef7c596
Harun Ćerim <harun.cerim@outlook.com>
2020-03-26 00:47:06 +0100

    added async capability for maybe monad mapping

--- Funk/Types/Maybe/Maybe.cs
+++ Funk/Types/Maybe/Maybe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
 using Funk.Exceptions;
 
 namespace Funk
@@ -79,9 +80,20 @@ namespace Funk
         public Maybe<R> Map<R>(Func<T, R> selector) => FlatMap(t => selector(t).AsMaybe());
 
         /// <summary>
+        /// Maps not empty Maybe to the Task of new Maybe of the selector. Otherwise, returns Task of empty Maybe of the selector.
+        /// Use FlatMap if you have nested Maybes.
+        /// </summary>
+        public async Task<Maybe<R>> Map<R>(Func<T, Task<R>> selector) => await FlatMap(async t => (await selector(t)).AsMaybe());
+
+        /// <summary>
         /// Maps not empty Maybe to the new Maybe of the selector. Otherwise, returns empty Maybe of the selector.
         /// </summary>
         public Maybe<R> FlatMap<R>(Func<T, Maybe<R>> selector) => Match(_ => Maybe.Empty, selector);
+
+        /// <summary>
+        /// Maps not empty Maybe to the Task of new Maybe of the selector. Otherwise, returns Task of empty Maybe of the selector.
+        /// </summary>
+        public async Task<Maybe<R>> FlatMap<R>(Func<T, Task<Maybe<R>>> selector) => await Match(async _ => await Task.Run(() => new Maybe<R>()), async v => await selector(v));
 
         /// <summary>
         /// Returns not empty value of Maybe or throws EmptyValueException (unless specified explicitly).
