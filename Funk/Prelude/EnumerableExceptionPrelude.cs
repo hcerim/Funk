e79aa0d4d16c36f43d5137aee764f92f6265adfc
Harun Ćerim <harun.cerim@outlook.com>
2020-03-27 14:20:55 +0100

    minor modifications

---
+++ Funk/Prelude/EnumerableExceptionPrelude.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Funk.Exceptions;
+
+namespace Funk
+{
+    public static partial class Prelude
+    {
+        public static EnumerableException exception(Exception exception) => exception.ToException();
+    }
+}
--- Funk/Prelude/RecordPrelude.cs
+++ Funk/Prelude/RecordPrelude.cs
@@ -8,30 +8,30 @@ namespace Funk
         /// Creates a record of 1.
         /// </summary>
         [Pure]
-        public static Record<T1> record<T1>(T1 t1) => Record.Create(t1);
+        public static Record<T1> record<T1>(T1 t1) => t1.ToRecord();
 
         /// <summary>
         /// Creates a record of 2.
         /// </summary>
         [Pure]
-        public static Record<T1, T2> record<T1, T2>(T1 t1, T2 t2) => Record.Create(t1, t2);
+        public static Record<T1, T2> record<T1, T2>(T1 t1, T2 t2) => (t1, t2).ToRecord();
 
         /// <summary>
         /// Creates a record of 3.
         /// </summary>
         [Pure]
-        public static Record<T1, T2, T3> record<T1, T2, T3>(T1 t1, T2 t2, T3 t3) => Record.Create(t1, t2, t3);
+        public static Record<T1, T2, T3> record<T1, T2, T3>(T1 t1, T2 t2, T3 t3) => (t1, t2, t3).ToRecord();
 
         /// <summary>
         /// Creates a record of 4.
         /// </summary>
         [Pure]
-        public static Record<T1, T2, T3, T4> record<T1, T2, T3, T4>(T1 t1, T2 t2, T3 t3, T4 t4) => Record.Create(t1, t2, t3, t4);
+        public static Record<T1, T2, T3, T4> record<T1, T2, T3, T4>(T1 t1, T2 t2, T3 t3, T4 t4) => (t1, t2, t3, t4).ToRecord();
 
         /// <summary>
         /// Creates a record of 5.
         /// </summary>
         [Pure]
-        public static Record<T1, T2, T3, T4, T5> record<T1, T2, T3, T4, T5>(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) => Record.Create(t1, t2, t3, t4, t5);
+        public static Record<T1, T2, T3, T4, T5> record<T1, T2, T3, T4, T5>(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) => (t1, t2, t3, t4, t5).ToRecord();
     }
 }
