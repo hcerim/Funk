562274c2f5e3ff54f70df0651fb2d6e4c3a57863
Harun Ćerim <harun.cerim@outlook.com>
2020-03-28 19:56:06 +0100

    flatmap async simplification

--- Funk/Types/Maybe/Maybe.cs
+++ Funk/Types/Maybe/Maybe.cs
@@ -98,7 +98,14 @@ namespace Funk
         /// Structure-preserving map.
         /// Binds not empty Maybe to the Task of new Maybe of the selector. Otherwise, returns Task of empty Maybe of the selector.
         /// </summary>
-        public async Task<Maybe<R>> FlatMap<R>(Func<T, Task<Maybe<R>>> selector) => await Match(_ => Task.Run(() => new Maybe<R>()), selector);
+        public async Task<Maybe<R>> FlatMap<R>(Func<T, Task<Maybe<R>>> selector)
+        {
+            if (Discriminator.SafeEquals(1))
+            {
+                return await selector((T)Value);
+            }
+            return empty;
+        }
 
         /// <summary>
         /// Returns not empty value of Maybe or throws EmptyValueException (unless specified explicitly).
