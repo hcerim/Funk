11c3d22a0824ccc3ddd7cc1a89848e414e779738
Harun Ćerim <harun.cerim@outlook.com>
2020-03-29 14:01:07 +0200

    minor refactoring

--- Funk/Extensions/InternalExt.cs
+++ Funk/Extensions/InternalExt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
 using Funk.Exceptions;
 
 namespace Funk.Internal
@@ -27,6 +28,30 @@ namespace Funk.Internal
                 _ => throw UnhandledException,
                 e => e(Unit.Value)
             );
+        }
+
+        internal static Exc<T, E> TryCatch<T, E>(this Func<Unit, T> operation) where E : Exception
+        {
+            try
+            {
+                return new Exc<T, E>(operation(Unit.Value));
+            }
+            catch (E e)
+            {
+                return new Exc<T, E>(e);
+            }
+        }
+
+        internal static async Task<Exc<T, E>> TryCatch<T, E>(this Func<Unit, Task<T>> operation) where E : Exception
+        {
+            try
+            {
+                return new Exc<T, E>(await operation(Unit.Value));
+            }
+            catch (E e)
+            {
+                return new Exc<T, E>(e);
+            }
         }
 
         private static void Throw(this Func<Unit, Exception> otherwiseThrow, Func<Unit, FunkException> exception)
