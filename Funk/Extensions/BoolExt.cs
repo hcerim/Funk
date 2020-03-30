c71dd38950f2acec6615e677582ba41b0cfaf6ab
Harun Ćerim <harun.cerim@outlook.com>
2020-03-30 08:59:04 +0200

    added bool tests

--- Funk/Extensions/BoolExt.cs
+++ Funk/Extensions/BoolExt.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using static Funk.Prelude;
 
 namespace Funk
 {
     public static class BoolExt
     {
-        public static T Match<T>(this bool item, Func<Unit, T> ifFalse, Func<Unit, T> ifTrue) => item is false ? ifFalse(Unit.Value) : ifTrue(Unit.Value);
+        public static T Match<T>(this bool item, Func<Unit, T> ifFalse, Func<Unit, T> ifTrue) => item ? ifTrue(Unit.Value) : ifFalse(Unit.Value);
 
         public static void Match(this bool item, Action<Unit> ifFalse, Action<Unit> ifTrue)
         {
@@ -16,6 +17,26 @@ namespace Funk
             {
                 ifFalse(Unit.Value);
             }
+        }
+
+        /// <summary>
+        /// If false, Maybe will be empty.
+        /// </summary>
+        public static Maybe<bool> IsTrue(this bool item)
+        {
+            if (item)
+            {
+                return true;
+            }
+            return empty;
+        }
+
+        /// <summary>
+        /// If false or null, Maybe will be empty.
+        /// </summary>
+        public static Maybe<bool> IsTrue(this bool? item)
+        {
+            return item is null ? empty : IsTrue(item.Value);
         }
     }
 }
