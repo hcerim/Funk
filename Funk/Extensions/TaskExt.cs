5c231a4e2220b9254352e83a5619d4fdc6805169
Harun Ćerim <harun.cerim@outlook.com>
2020-03-31 14:59:15 +0200

    new extension method for task without result

---
+++ Funk/Extensions/TaskExt.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Funk.Extensions
+{
+    public static class TaskExt
+    {
+        public static Task<Unit> WithResult(this Task task)
+        {
+            return Task.Run(async () =>
+            {
+                await task;
+                return Unit.Value;
+            });
+        }
+    }
+}
