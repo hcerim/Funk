e76c991ee038028d581bb85e344bf28d52120853
Harun Ćerim <harun.cerim@outlook.com>
2020-04-01 15:28:03 +0200

    modified csproj with list prelude

---
+++ Funk/Prelude/ListPrelude.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Funk
+{
+    public static partial class Prelude
+    {
+        public static IImmutableList<T> lst<T>(params T[] items) => items.Map();
+        public static IImmutableList<T> lst<T>(IEnumerable<T> enumerable) => enumerable.Map();
+    }
+}
