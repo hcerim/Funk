9d8d2599b9735ee7953b50e54d5840bbb9a7000c
Harun Ćerim <harun.cerim@outlook.com>
2020-04-05 19:47:38 +0200

    dispose methods added

---
+++ Funk.Tests/ExtensionsTests/DisposableTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using Xunit;
+using static Funk.Prelude;
+
+namespace Funk.Tests.ExtensionsTests
+{
+    public class DisposableTests : Test
+    {
+        [Fact]
+        public void Dispose()
+        {
+            UnitTest(
+                _ => new HttpClient(),
+                c =>
+                {
+                    c.DisposeAfter(cl => cl.CancelPendingRequests());
+                    return c;
+                },
+                c =>
+                {
+                    Assert.Throws<ObjectDisposedException>(act(c.CancelPendingRequests));
+                }
+            );
+        }
+    }
+}
