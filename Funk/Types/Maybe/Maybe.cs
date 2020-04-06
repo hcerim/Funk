d2904f30d7cd3f669bf95dc698fc7ccc58f89d2d
Harun Ćerim <harun.cerim@outlook.com>
2020-04-06 20:33:01 +0200

    moved factories

--- Funk/Types/Maybe/Maybe.cs
+++ Funk/Types/Maybe/Maybe.cs
@@ -10,6 +10,31 @@ namespace Funk
     /// Maybe monad.
     /// Type that represents the possible absence of data with appropriate handling.
     /// </summary>
+    public struct Maybe
+    {
+        /// <summary>
+        /// Creates a Maybe of item.
+        /// </summary>
+        [Pure]
+        public static Maybe<T> Create<T>(T item) => new Maybe<T>(item);
+
+        /// <summary>
+        /// Creates a Maybe of nullable item.
+        /// </summary>
+        [Pure]
+        public static Maybe<T> Create<T>(T? item) where T : struct => item.IsNotNull() ? new Maybe<T>((T)item) : empty;
+
+        /// <summary>
+        /// Creates empty Maybe.
+        /// </summary>
+        [Pure]
+        public static Maybe<T> Empty<T>() => new Maybe<T>();
+    }
+
+    /// <summary>
+    /// Maybe monad.
+    /// Type that represents the possible absence of data with appropriate handling.
+    /// </summary>
     public struct Maybe<T> : IEquatable<Maybe<T>>
     {
         internal Maybe(T item)
