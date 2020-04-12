213d5bb9628fcf379621abb3ac5c48027bf46aba
Harun Ćerim <harun.cerim@outlook.com>
2020-04-12 14:00:14 +0200

    added task prelude

---
+++ Funk/Prelude/TaskPrelude.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Funk
+{
+    public static partial class Prelude
+    {
+        public static Task<T> result<T>(T item) => Task.FromResult(item);
+
+        public static async Task run(Action action) => await Task.Run(action).ConfigureAwait(false);
+
+        public static async Task run(Action action, CancellationToken token) => await Task.Run(action, token).ConfigureAwait(false);
+
+        public static async Task<T> run<T>(Func<T> action) => await Task.Run(action).ConfigureAwait(false);
+
+        public static async Task<T> run<T>(Func<T> action, CancellationToken token) => await Task.Run(action, token).ConfigureAwait(false);
+
+        public static async Task run(Func<Task> action) => await Task.Run(action).ConfigureAwait(false);
+
+        public static async Task run(Func<Task> action, CancellationToken token) => await Task.Run(action, token).ConfigureAwait(false);
+
+        public static async Task<T> run<T>(Func<Task<T>> action) => await Task.Run(action).ConfigureAwait(false);
+
+        public static async Task<T> run<T>(Func<Task<T>> action, CancellationToken token) => await Task.Run(action, token).ConfigureAwait(false);
+    }
+}
