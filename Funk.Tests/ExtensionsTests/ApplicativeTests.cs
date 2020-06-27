6b15629f5f7c121c932c0a3cd61e3810fa29d733
Harun Ćerim <harun.cerim@outlook.com>
2020-06-28 00:09:35 +0200

    added arbitrary data lifter in property-based testing

--- Funk.Tests/ExtensionsTests/ApplicativeTests.cs
+++ Funk.Tests/ExtensionsTests/ApplicativeTests.cs
@@ -3,26 +3,26 @@ using FsCheck.Xunit;
 using Xunit;
 using static Funk.Prelude;
 
-namespace Funk.Tests.ExtensionsTests
+namespace Funk.Tests
 {
     public class ApplicativeTests : Test
     {
-        [Property]
-        public void ApplicativeLawForMaybe(int first, int second)
+        [Property(Arbitrary = new[] { typeof(ArbitraryLifter) })]
+        public void ApplicativeLawForMaybe(Maybe<int> first, Maybe<int> second)
         {
             UnitTest(
                 _ => func((int a, int b) => a + b),
-                f => rec(first.AsMaybe().Map(f.Apply).Apply(second), f.AsMaybe().Apply(first).Apply(second)),
+                f => rec(first.Map(f.Apply).Apply(second), f.AsMaybe().Apply(first).Apply(second)),
                 r => Assert.Equal(r.Item1, r.Item2)
             );
         }
 
-        [Property]
-        public void ApplicativeLawForExc(int first, int second)
+        [Property(Arbitrary = new[] { typeof(ArbitraryLifter) })]
+        public void ApplicativeLawForExc(Exc<int, DivideByZeroException> first, Exc<int, DivideByZeroException> second)
         {
             UnitTest(
                 _ => func((int a, int b) => a / b),
-                f => rec(success<int, Exception>(first).Map(f.Apply).Apply(second), success<Func<int, int, int>, Exception>(f).Apply(first).Apply(second)),
+                f => rec(first.Map(f.Apply).Apply(second), success<Func<int, int, int>, DivideByZeroException>(f).Apply(first).Apply(second)),
                 r => Assert.Equal(r.Item1.Success, r.Item2.Success)
             );
         }
