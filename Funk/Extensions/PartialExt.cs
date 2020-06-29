acb541b28ddb3a8ac6485c75cdabd4dd6cbdb3fa
Harun Ćerim <harun.cerim@outlook.com>
2020-06-29 20:52:20 +0200

    added aply for final result

--- Funk/Extensions/PartialExt.cs
+++ Funk/Extensions/PartialExt.cs
@@ -4,6 +4,8 @@ namespace Funk
 {
     public static class PartialExt
     {
+        public static R Apply<T1, R>(this Func<T1, R> function, T1 t1) => function(t1);
+
         public static Func<T2, R> Apply<T1, T2, R>(this Func<T1, T2, R> function, T1 t1) => t2 => function(t1, t2);
 
         public static Func<T2, T3, R> Apply<T1, T2, T3, R>(this Func<T1, T2, T3, R> function, T1 t1) => (t2, t3) => function(t1, t2, t3);
