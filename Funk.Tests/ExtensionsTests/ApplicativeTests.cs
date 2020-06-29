56ed50c92e1b2689f06029ead32a1ae21ffbee9e
Harun Ćerim <harun.cerim@outlook.com>
2020-06-29 17:36:17 +0200

    added applicative for contract and tests for exc

--- Funk.Tests/ExtensionsTests/ApplicativeTests.cs
+++ Funk.Tests/ExtensionsTests/ApplicativeTests.cs
@@ -26,5 +26,33 @@ namespace Funk.Tests
                 r => Assert.Equal(r.Item1.Success, r.Item2.Success)
             );
         }
+
+        [Property(Arbitrary = new[] { typeof(ArbitraryLifter) })]
+        public void ApplicativeContractLawForExc(Exc<int, Exception> first, Exc<int, Exception> second, Exc<int, Exception> third)
+        {
+            UnitTest(
+                _ => func((int a, int b, int c) => a / b * c),
+                f => rec(first.Map(f.Apply).ApplyContract(second).ApplyContract(third), success<Func<int, int, int, int>, Exception>(f).ApplyContract(first).ApplyContract(second).ApplyContract(third)),
+                r => Assert.Equal(r.Item1.Success, r.Item2.Success)
+            );
+        }
+
+        [Fact]
+        public void ApplicativeContract()
+        {
+            UnitTest(
+                _ => func((int a, int b, int c) => a + b + c),
+                f => success<Func<int, int, int, int>, Exception>(f)
+                    .ApplyContract(failure<int, Exception>(new DivideByZeroException("One")))
+                    .ApplyContract(failure<int, Exception>(new InvalidCastException("Two")))
+                    .ApplyContract(failure<int, Exception>(new InvalidOperationException("Three"))),
+                r =>
+                {
+                    Assert.True(r.Failure.NotEmpty);
+                    Assert.Equal(3, r.NestedFailures.UnsafeGet().Count);
+                    Assert.Equal("One", r.RootFailure.UnsafeGet().Message);
+                }
+            );
+        }
     }
 }
--- Funk/Extensions/ApplicativeExt.cs
+++ Funk/Extensions/ApplicativeExt.cs
@@ -16,12 +16,117 @@ namespace Funk
 
         public static Exc<R, E> Apply<T1, R, E>(this Exc<Func<T1, R>, E> function, Exc<T1, E> exceptional) where E : Exception => function.FlatMap(exceptional.Map);
 
+        /// <summary>
+        /// Returns success if both are successful. Otherwise, returns error/s as part of EnumerableException.
+        /// In case of continuation use Apply instead.
+        /// </summary>
+        public static Exc<R, E> ApplyContract<T1, R, E>(this Exc<Func<T1, R>, E> function, Exc<T1, E> exceptional) where E : Exception
+        {
+            return function.Match(
+                _ => Exc.Empty<R, E>(),
+                f => exceptional.Match(
+                    __ => Exc.Empty<R, E>(),
+                    v => Exc.Create<R, E>(_ => f(v)),
+                    Exc.Failure<R, E>
+                ),
+                e => exceptional.Match(
+                    __ => Exc.Empty<R, E>(),
+                    v => Exc.Failure<R, E>(e),
+                    ee => Exc.Failure<R, E>(e.Merge(ee))
+                )
+            );
+        }
+
         public static Exc<Func<T2, R>, E> Apply<T1, T2, R, E>(this Exc<Func<T1, T2, R>, E> function, Exc<T1, E> exceptional) where E : Exception => function.FlatMap(f => exceptional.Map(f.Apply));
+
+        /// <summary>
+        /// Returns success if both are successful. Otherwise, returns error/s as part of EnumerableException.
+        /// In case of continuation use Apply instead.
+        /// </summary>
+        public static Exc<Func<T2, R>, E> ApplyContract<T1, T2, R, E>(this Exc<Func<T1, T2, R>, E> function, Exc<T1, E> exceptional) where E : Exception
+        {
+            return function.Match(
+                _ => Exc.Empty<Func<T2, R>, E>(),
+                f => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, R>, E>(),
+                    v => Exc.Create<Func<T2, R>, E>(_ => f.Apply(v)),
+                    Exc.Failure<Func<T2, R>, E>
+                ),
+                e => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, R>, E>(),
+                    v => Exc.Failure<Func<T2, R>, E>(e),
+                    ee => Exc.Failure<Func<T2, R>, E>(e.Merge(ee))
+                )
+            );
+        }
 
         public static Exc<Func<T2, T3, R>, E> Apply<T1, T2, T3, R, E>(this Exc<Func<T1, T2, T3, R>, E> function, Exc<T1, E> exceptional) where E : Exception => function.FlatMap(f => exceptional.Map(f.Apply));
 
+        /// <summary>
+        /// Returns success if both are successful. Otherwise, returns error/s as part of EnumerableException.
+        /// In case of continuation use Apply instead.
+        /// </summary>
+        public static Exc<Func<T2, T3, R>, E> ApplyContract<T1, T2, T3, R, E>(this Exc<Func<T1, T2, T3, R>, E> function, Exc<T1, E> exceptional) where E : Exception
+        {
+            return function.Match(
+                _ => Exc.Empty<Func<T2, T3, R>, E>(),
+                f => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, T3, R>, E>(),
+                    v => Exc.Create<Func<T2, T3, R>, E>(_ => f.Apply(v)),
+                    Exc.Failure<Func<T2, T3, R>, E>
+                ),
+                e => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, T3, R>, E>(),
+                    v => Exc.Failure<Func<T2, T3, R>, E>(e),
+                    ee => Exc.Failure<Func<T2, T3, R>, E>(e.Merge(ee))
+                )
+            );
+        }
+
         public static Exc<Func<T2, T3, T4, R>, E> Apply<T1, T2, T3, T4, R, E>(this Exc<Func<T1, T2, T3, T4, R>, E> function, Exc<T1, E> exceptional) where E : Exception => function.FlatMap(f => exceptional.Map(f.Apply));
 
+        /// <summary>
+        /// Returns success if both are successful. Otherwise, returns error/s as part of EnumerableException.
+        /// In case of continuation use Apply instead.
+        /// </summary>
+        public static Exc<Func<T2, T3, T4, R>, E> ApplyContract<T1, T2, T3, T4, R, E>(this Exc<Func<T1, T2, T3, T4, R>, E> function, Exc<T1, E> exceptional) where E : Exception
+        {
+            return function.Match(
+                _ => Exc.Empty<Func<T2, T3, T4, R>, E>(),
+                f => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, T3, T4, R>, E>(),
+                    v => Exc.Create<Func<T2, T3, T4, R>, E>(_ => f.Apply(v)),
+                    Exc.Failure<Func<T2, T3, T4, R>, E>
+                ),
+                e => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, T3, T4, R>, E>(),
+                    v => Exc.Failure<Func<T2, T3, T4, R>, E>(e),
+                    ee => Exc.Failure<Func<T2, T3, T4, R>, E>(e.Merge(ee))
+                )
+            );
+        }
+
         public static Exc<Func<T2, T3, T4, T5, R>, E> Apply<T1, T2, T3, T4, T5, R, E>(this Exc<Func<T1, T2, T3, T4, T5, R>, E> function, Exc<T1, E> exceptional) where E : Exception => function.FlatMap(f => exceptional.Map(f.Apply));
+
+        /// <summary>
+        /// Returns success if both are successful. Otherwise, returns error/s as part of EnumerableException.
+        /// In case of continuation use Apply instead.
+        /// </summary>
+        public static Exc<Func<T2, T3, T4, T5, R>, E> ApplyContract<T1, T2, T3, T4, T5, R, E>(this Exc<Func<T1, T2, T3, T4, T5, R>, E> function, Exc<T1, E> exceptional) where E : Exception
+        {
+            return function.Match(
+                _ => Exc.Empty<Func<T2, T3, T4, T5, R>, E>(),
+                f => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, T3, T4, T5, R>, E>(),
+                    v => Exc.Create<Func<T2, T3, T4, T5, R>, E>(_ => f.Apply(v)),
+                    Exc.Failure<Func<T2, T3, T4, T5, R>, E>
+                ),
+                e => exceptional.Match(
+                    __ => Exc.Empty<Func<T2, T3, T4, T5, R>, E>(),
+                    v => Exc.Failure<Func<T2, T3, T4, T5, R>, E>(e),
+                    ee => Exc.Failure<Func<T2, T3, T4, T5, R>, E>(e.Merge(ee))
+                )
+            );
+        }
     }
 }
