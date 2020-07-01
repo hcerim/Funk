d363492f676ffbbdeca59fa05b545438d98b47f7
Harun Ćerim <harun.cerim@outlook.com>
2020-07-02 01:38:26 +0200

    m fix

--- Funk/Types/Pattern.cs
+++ Funk/Types/Pattern.cs
@@ -12,14 +12,14 @@ namespace Funk
     {
         public static Pattern<T> Match<T>(params (object @case, Func<object, T> function)[] sequence) => new Pattern<T>(sequence.WhereOrDefault(r => r.@case.IsNotNull() && r.function.IsNotNull()).Map(l => l.Map(r => r.ToRecord())).GetOrEmpty());
 
-        public static Pattern<R> Match<T, R>(params (object @case, Func<T, R> function)[] sequence) => new Pattern<R>(sequence.WhereOrDefault(r => r.@case.IsNotNull() && r.function.IsNotNull()).Map(l => l.Map(r => rec<object, Func<object, R>>(r.@case, o => r.function((T)r.@case)))).GetOrEmpty());
+        public static Pattern<R> Match<T, R>(params (T @case, Func<T, R> function)[] sequence) => new Pattern<R>(sequence.WhereOrDefault(r => r.@case.IsNotNull() && r.function.IsNotNull()).Map(l => l.Map(r => rec<object, Func<object, R>>(r.@case, o => r.function(r.@case)))).GetOrEmpty());
     }
 
     public static class AsyncPattern
     {
         public static AsyncPattern<T> Match<T>(params (object @case, Func<object, Task<T>> function)[] sequence) => new AsyncPattern<T>(sequence.WhereOrDefault(r => r.@case.IsNotNull() && r.function.IsNotNull()).Map(l => l.Map(r => r.ToRecord())).GetOrEmpty());
 
-        public static AsyncPattern<R> Match<T, R>(params (object @case, Func<T, Task<R>> function)[] sequence) => new AsyncPattern<R>(sequence.WhereOrDefault(r => r.@case.IsNotNull() && r.function.IsNotNull()).Map(l => l.Map(r => rec<object, Func<object, Task<R>>>(r.@case, o => r.function((T)r.@case)))).GetOrEmpty());
+        public static AsyncPattern<R> Match<T, R>(params (T @case, Func<T, Task<R>> function)[] sequence) => new AsyncPattern<R>(sequence.WhereOrDefault(r => r.@case.IsNotNull() && r.function.IsNotNull()).Map(l => l.Map(r => rec<object, Func<object, Task<R>>>(r.@case, o => r.function(r.@case)))).GetOrEmpty());
     }
 
     public readonly struct Pattern<T>
