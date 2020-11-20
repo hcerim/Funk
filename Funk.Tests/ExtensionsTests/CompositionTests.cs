2d1f81cee4716015a0ebf4e70692a9424198c92a
Harun Ćerim <harun.cerim@outlook.com>
2020-11-20 13:18:31 +0100

    compose naming changes

--- Funk.Tests/ExtensionsTests/CompositionTests.cs
+++ Funk.Tests/ExtensionsTests/CompositionTests.cs
@@ -10,7 +10,7 @@ namespace Funk.Tests
         {
             UnitTest(
                 _ => func((int i) => i * 3),
-                f => f.Compose((int i) => i * 2),
+                f => f.ComposeRight((int i) => i * 2),
                 f => Assert.Equal(6, f.Apply(1))
             );
         }
@@ -20,7 +20,7 @@ namespace Funk.Tests
         {
             UnitTest(
                 _ => func((int i) => i - 1),
-                f => f.Compose((int i) => i * 2),
+                f => f.ComposeRight((int i) => i * 2),
                 f => Assert.Equal(1, f.Apply(1))
             );
         }
@@ -30,7 +30,7 @@ namespace Funk.Tests
         {
             UnitTest(
                 _ => func((int i) => i - 1),
-                f => f.ComposeInvert(i => i * 2),
+                f => f.ComposeLeft(i => i * 2),
                 f => Assert.Equal(0, f.Apply(1))
             );
         }
@@ -40,7 +40,7 @@ namespace Funk.Tests
         {
             UnitTest(
                 _ => func((int i) => i * 3),
-                f => f.Compose((int i) => i * 2).ComposeInvert(i => $"{i}"),
+                f => f.ComposeRight((int i) => i * 2).ComposeLeft(i => $"{i}"),
                 f => Assert.Equal("6", f.Apply(1))
             );
         }
@@ -51,16 +51,14 @@ namespace Funk.Tests
             UnitTest(
                 _ => func((int i) => i * 3),
                 f => f
-                    .Compose((int i) => i * 2)
-                    .ComposeInvert(i => i - 3)
-                    .Compose((int i) => i * 2)
-                    .ComposeInvert(i => i / 3)
-                    .Compose((int i) => i / 2)
-                    .Compose((int i) => i / 2)
-                    .ComposeInvert(i => i * 2),
-                // execution goes from the latest compose function through the initiator to the last composeInvert function.
-                // pipe syntax (execution order): (i/2) -> (i/2) -> (i*2) -> (i*2) -> (i*3) -> (i-3) -> (i/3) -> (i*2)
-                // composition syntax (code order): (i*3) >> (i*2) << (i-3) >> (i*2) << (i/3) >> (i/2) >> (i/2) << (i*2)
+                    .ComposeRight((int i) => i * 2)
+                    .ComposeLeft(i => i - 3)
+                    .ComposeRight((int i) => i * 2)
+                    .ComposeLeft(i => i / 3)
+                    .ComposeRight((int i) => i / 2)
+                    .ComposeRight((int i) => i / 2)
+                    .ComposeLeft(i => i * 2),
+                // execution goes from the latest ComposeRight function through the initiator function to the last ComposeLeft function.
                 f =>
                 {
                     Assert.Equal(14, f.Apply(8));
--- Funk/Extensions/CompositionExt.cs
+++ Funk/Extensions/CompositionExt.cs
@@ -4,8 +4,14 @@ namespace Funk
 {
     public static class CompositionExt
     {
-        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T2, T3> f1, Func<T1, T2> f2) => i => f1(f2(i));
+        /// <summary>
+        /// Composes 2 functions from right to left.
+        /// </summary>
+        public static Func<T1, T3> ComposeRight<T1, T2, T3>(this Func<T2, T3> f1, Func<T1, T2> f2) => i => f1(f2(i));
         
-        public static Func<T1, T3> ComposeInvert<T1, T2, T3>(this Func<T1, T2> f1, Func<T2, T3> f2) => i => f2(f1(i));
+        /// <summary>
+        /// Composes 2 functions from left to right.
+        /// </summary>
+        public static Func<T1, T3> ComposeLeft<T1, T2, T3>(this Func<T1, T2> f1, Func<T2, T3> f2) => i => f2(f1(i));
     }
 }
