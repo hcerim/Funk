b9fa40295e1c1830bfa79f3171f9ae33e431a8a7
Harun Ćerim <harun.cerim@outlook.com>
2021-01-19 13:21:18 +0100

    added async extensions for record

--- Funk/Extensions/RecordExt.cs
+++ Funk/Extensions/RecordExt.cs
@@ -1,9 +1,61 @@
-﻿using System.Diagnostics.Contracts;
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
 
 namespace Funk
 {
     public static class RecordExt
     {
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 1.
+        /// </summary>
+        public static async Task<Record<R1>> FlatMapAsync<T1, R1>(this Task<Record<T1>> record, Func<T1, Task<Record<R1>>> selector) => await (await record).FlatMapAsync(selector).ConfigureAwait(false);
+
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 1.
+        /// </summary>
+        public static async Task<Record<R1>> MapAsync<T1, R1>(this Task<Record<T1>> record, Func<T1, Task<R1>> selector) => await (await record).MapAsync(selector).ConfigureAwait(false);
+        
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 2.
+        /// </summary>
+        public static async Task<Record<R1, R2>> FlatMapAsync<T1, T2, R1, R2>(this Task<Record<T1, T2>> record, Func<T1, T2, Task<Record<R1, R2>>> selector) => await (await record).FlatMapAsync(selector).ConfigureAwait(false);
+
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 2.
+        /// </summary>
+        public static async Task<Record<R1, R2>> MapAsync<T1, T2, R1, R2>(this Task<Record<T1, T2>> record, Func<T1, T2, Task<(R1, R2)>> selector) => await (await record).MapAsync(selector).ConfigureAwait(false);
+        
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 3.
+        /// </summary>
+        public static async Task<Record<R1, R2, R3>> FlatMapAsync<T1, T2, T3, R1, R2, R3>(this Task<Record<T1, T2, T3>> record, Func<T1, T2, T3, Task<Record<R1, R2, R3>>> selector) => await (await record).FlatMapAsync(selector).ConfigureAwait(false);
+
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 3.
+        /// </summary>
+        public static async Task<Record<R1, R2, R3>> MapAsync<T1, T2, T3, R1, R2, R3>(this Task<Record<T1, T2, T3>> record, Func<T1, T2, T3, Task<(R1, R2, R3)>> selector) => await (await record).MapAsync(selector).ConfigureAwait(false);
+        
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 4.
+        /// </summary>
+        public static async Task<Record<R1, R2, R3, R4>> FlatMapAsync<T1, T2, T3, T4, R1, R2, R3, R4>(this Task<Record<T1, T2, T3, T4>> record, Func<T1, T2, T3, T4, Task<Record<R1, R2, R3, R4>>> selector) => await (await record).FlatMapAsync(selector).ConfigureAwait(false);
+
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 4.
+        /// </summary>
+        public static async Task<Record<R1, R2, R3, R4>> MapAsync<T1, T2, T3, T4, R1, R2, R3, R4>(this Task<Record<T1, T2, T3, T4>> record, Func<T1, T2, T3, T4, Task<(R1, R2, R3, R4)>> selector) => await (await record).MapAsync(selector).ConfigureAwait(false);
+        
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 5.
+        /// </summary>
+        public static async Task<Record<R1, R2, R3, R4, R5>> FlatMapAsync<T1, T2, T3, T4, T5, R1, R2, R3, R4, R5>(this Task<Record<T1, T2, T3, T4, T5>> record, Func<T1, T2, T3, T4, T5, Task<Record<R1, R2, R3, R4, R5>>> selector) => await (await record).FlatMapAsync(selector).ConfigureAwait(false);
+
+        /// <summary>
+        /// Structure-preserving map. Maps corresponding record items to the new Record of 5.
+        /// </summary>
+        public static async Task<Record<R1, R2, R3, R4, R5>> MapAsync<T1, T2, T3, T4, T5, R1, R2, R3, R4, R5>(this Task<Record<T1, T2, T3, T4, T5>> record, Func<T1, T2, T3, T4, T5, Task<(R1, R2, R3, R4, R5)>> selector) => await (await record).MapAsync(selector).ConfigureAwait(false);
+        
         /// <summary>
         /// Creates a record of 1.
         /// </summary>
